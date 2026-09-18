e4b12e20fd59bb4771d2375da50086fecc8c3aa8
CodeBreaker <jinal4474@gmail.com>
2026-09-19 00:00:39 +0530

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-0

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    specialArray(nums) {
+        for(let i=1;i<=nums.length;i++){
+            let count = 0;
+            for(let j=0;j<nums.length;j++){
+                if(nums[j]>=i){
+                    count++;
+                }
+            }
+            if(count==i){
+                return count;
+            }            
+        }
+        return -1;
+    }
+}
